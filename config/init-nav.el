520d1ea904290cd1bb81194b0df57530150246f8
Alec <alecfvn@gmail.com>
2019-07-08 23:28:22 +0200

    added magit status global key

--- config/init-nav.el
+++ config/init-nav.el
@@ -7,8 +7,12 @@
 ;; Silversearcher
 (use-package ag :ensure t)
 
+
 ;; Git version control
-(use-package magit :ensure t)
+(use-package magit
+  :ensure t
+  :init
+  (global-set-key (kbd "C-x g") 'magit-status))
 
 ;; Provides versions of common Emacs commands which use Ivy
 (use-package counsel :ensure t)
