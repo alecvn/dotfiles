e65359e317d27a132993fd79424ca2d0fd033771
alec <alec@implicitdesign.co.za>
2019-10-31 12:38:17 +0200

    notes on ivy and some additions

--- config/init-completion.el
+++ config/init-completion.el
@@ -13,6 +13,7 @@
   :config
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
+  (setq ivy-use-selectable-prompt t)
   (setq ivy-initial-inputs-alist nil)
   (setq ivy-re-builders-alist
 	;; allow input not in order
