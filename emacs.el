e6f74b0ff26b666af9d145223708ce1dd4ffa983
Alec <alecfvn@gmail.com>
2020-04-20 18:38:55 +0200

    fixed font size

--- emacs.el
+++ emacs.el
@@ -135,6 +135,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "gray12" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
  '(all-the-icons-dired-dir-face ((t (:foreground "steel blue")))))
 
 (set-face-attribute 'default (selected-frame) :height 100)
