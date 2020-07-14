03dc7c68681cddc6b86cc610e6709fb140bc587b
andrew <andrew@impd.co.za>
2020-07-14 11:43:17 +0200

    updated python hooks and setup initial version of python type hinting with mypy

--- config/init-python.el
+++ config/init-python.el
@@ -2,6 +2,10 @@
   :ensure t
   )
 
+(use-package pyimport
+  :ensure t
+  )
+
 (use-package blacken
   :ensure t
   ;;:ensure-system-package (black . "pip3 install black")
@@ -31,9 +35,8 @@
   :ensure t
   :init
   (setq python-shell-interpreter "python3")
-  (add-hook 'python-mode-hook
-          '(lambda()
-             (add-hook 'before-save-hook 'py-isort-before-save)))
+  (add-hook 'before-save-hook 'pyimport-remove-unused)
+  (add-hook 'before-save-hook 'py-isort-before-save)
   (add-hook 'python-mode-hook 'blacken-mode)
   (fset 'pdb "import pdb; pdb.set_trace()")
   (fset 'ipdb "import ipdb; ipdb.set_trace()")
@@ -42,5 +45,52 @@
               ("C-c ." . jedi:goto-definition)
               ("C-c ," . jedi:goto-definition-pop-marker)
               )
-
   )
+
+;; flycheck
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode t)
+  )
+;; flycheck-pycheckers
+;; Allows multiple syntax checkers to run in parallel on Python code
+;; Ideal use-case: pyflakes for syntax combined with mypy for typing
+(use-package flycheck-pycheckers
+  :after flycheck
+  :ensure t
+  :init
+  (with-eval-after-load 'flycheck
+    (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
+    )
+  (setq flycheck-pycheckers-checkers
+    '(
+      mypy3
+      pyflakes
+      )
+    )
+  )
+;; elpy
+(use-package elpy
+  :after poetry
+  :ensure t
+  :config
+  (elpy-enable)
+  (add-hook 'elpy-mode-hook 'poetry-tracking-mode) ;; optional if you're using Poetry
+  (setq elpy-rpc-virtualenv-path 'current)
+  (setq elpy-syntax-check-command "~/.pyenv/shims/pyflakes") ;; or replace with the path to your pyflakes binary
+  ;; allows Elpy to see virtualenv
+  (add-hook 'elpy-mode-hook
+        ;; pyvenv-mode
+        '(lambda ()
+           (pyvenv-mode +1)
+           )
+        )
+  ;; use flycheck instead of flymake
+  (when (load "flycheck" t t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+  )
+;; poetry
+(use-package poetry
+  :ensure t)
--- emacs.el
+++ emacs.el
@@ -52,3 +52,49 @@
 (load-file "~/.emacs.d/config/init-web.el")
 (load-file "~/.emacs.d/config/init-csharp.el")
 (load-file "~/.emacs.d/config/init-rest.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dashboard-banner-logo-title
+   "This is my Emacs. There are many like it, but this one is mine. My Emacs is my best friend. It is my life. I must master it as I master my life. My Emacs, without me, is useless. Without my Emacs, I am useless.")
+ '(dashboard-center-content t)
+ '(dashboard-navigator-buttons
+   '(((#("" 0 1
+	 (face
+	  (:family "Material Icons" :height 1.44)
+	  font-lock-face
+	  (:family "Material Icons" :height 1.44)
+	  display
+	  (raise -0.288)
+	  rear-nonsticky t))
+       "Refresh" "Refresh packages"
+       (lambda
+	 (&rest _)
+	 (package-refresh-contents t)))
+      (#("" 0 1
+	 (face
+	  (:family "FontAwesome" :height 1.44)
+	  font-lock-face
+	  (:family "FontAwesome" :height 1.44)
+	  display
+	  (raise 0.0)
+	  rear-nonsticky t))
+       "Update" "Update emacs"
+       (lambda
+	 (&rest _)
+	 (auto-package-update-now))))))
+ '(dashboard-set-file-icons t)
+ '(dashboard-set-footer nil)
+ '(dashboard-set-heading-icons t)
+ '(dashboard-set-navigator t)
+ '(package-selected-packages
+   '(pyimpsort poetry elpy flycheck-pycheckers yasnippet ws-butler wgrep web-mode use-package-ensure-system-package spacemacs-theme solarized-theme so-long smartparens shx shader-mode rvm robe restclient rbenv py-isort projectile-rails org omnisharp markdown-mode magit highlight-indent-guides git-timemachine flymake-ruby exec-path-from-shell enh-ruby-mode doom-themes doom-modeline diminish dashboard counsel-projectile company-tern company-jedi company-inf-ruby calmer-forest-theme buffer-move blacken auto-package-update all-the-icons-dired ag add-node-modules-path))
+ '(show-week-agenda-p t t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
